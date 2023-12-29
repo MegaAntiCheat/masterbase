257fc22f618b363f9999c91eec9cce9a984dc1ba
jayceslesar <jaycesles@gmail.com>
2023-12-28 21:58:34 -0500

    working session id endpoint

--- src/api/app.py
+++ src/api/app.py
@@ -1,18 +1,36 @@
-from litestar import Litestar, websocket_listener, get
-
 from uuid import uuid4
 
+from litestar import Litestar, get, websocket_listener
 
-@get("/session_id")
+
+def generate_uuid4_int() -> int:
+    """Seems useless, but makes testing easier."""
+    return uuid4().int
+
+
+@get("/session_id", sync_to_thread=False)
 def get_session_id() -> dict[str, int]:
-    session_id = uuid4().int
-    return session_id
+    """Return a session ID, as well as persist to database.
+
+    This is to help us know what is happening downstream:
+        - How many active upload sessions
+        - If the upload request contains a valid session ID
+        - Currently valid upload session ID's so client could reconnect
+
+    """
+    session_id = generate_uuid4_int()
+    return {"session_id": session_id}
+
 
 @websocket_listener("/demos")
 async def demo_session(data: dict[str, str]) -> dict[str, str]:
+    """Handle incoming data from a client upload.
+
+    Smart enough to know where to write data to based on the session ID
+    as to handle reconnecting/duplicates. Will eventually interface
+    with Minio as the file system abstraction layer.
+    """
     print(data)
 
 
-
-
-app = Litestar(route_handlers=[demo_session])
+app = Litestar(route_handlers=[get_session_id, demo_session])
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,15 @@
-from litestar.testing import TestClient
+from unittest import mock
 
 from api.app import app
+from litestar.testing import TestClient
 
+
+@mock.patch("api.app.generate_uuid4_int")
+def test_session_id(mock_uuid4) -> None:
+    with TestClient(app=app) as client:
+        mock_uuid4.return_value = 123
+        session_id = client.get("/session_id").json()
+        assert session_id["session_id"] == 123
 
 
 demo_data = {
@@ -9,7 +17,8 @@ demo_data = {
     "data": 123,
 }
 
-def test_health_check():
+
+def test_health_check() -> None:
     with TestClient(app=app) as client:
         ws_endpoint = client.websocket_connect("/demos")
 
