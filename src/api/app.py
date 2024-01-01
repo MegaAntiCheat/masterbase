9b02edbeeb5887243f2fa48fffb3bc778e4ce420
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2024-01-01 09:36:32 -0500

    feat(auth): provision API keys with steam sign in (#8)
    * feat(auth): provision API keys with steam sign in
    
    * fix docstring
    
    * better HTML response
    
    * update readme with API running instructions
    
    * make updated and created at the same

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,16 @@
 import base64
 import os
+from datetime import datetime, timezone
 from typing import BinaryIO, cast
+from urllib.parse import urlencode
 from uuid import uuid4
 
-from litestar import Litestar, get, websocket, websocket_listener
+import requests
+import sqlalchemy as sa
+from litestar import Litestar, MediaType, Request, get, websocket, websocket_listener
+from litestar.datastructures import State
 from litestar.di import Provide
+from litestar.response import Redirect
 from sqlalchemy import Engine, create_engine
 
 
@@ -21,7 +27,7 @@ def get_db_connection(app: Litestar) -> Engine:
     If it doesn't exist, creates it and saves it in on the application state object
     """
     if not getattr(app.state, "engine", None):
-        app.state.engine = websocket_listener(_make_db_uri())
+        app.state.engine = create_engine(_make_db_uri())
     return cast("Engine", app.state.engine)
 
 
@@ -143,8 +149,115 @@ async def demo_session(data: dict[str, str | bytes | int]) -> dict[str, str]:
     demo_manager.handle_demo_data(data)
 
 
+@get("/provision", sync_to_thread=False)
+def provision(request: Request) -> Redirect:
+    """Provision a login/API key.
+
+    Mostly stolen from https://github.com/TeddiO/pySteamSignIn/blob/master/pysteamsignin/steamsignin.py
+
+    Args:
+        request: current request object
+
+    Returns:
+        Redirect to the steam sign in
+    """
+    auth_params = {
+        "openid.ns": "http://specs.openid.net/auth/2.0",
+        "openid.mode": "checkid_setup",
+        "openid.return_to": f"{request.base_url}/provision_handler",
+        "openid.realm": f"{request.base_url}/provision_handler",
+        "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
+        "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
+    }
+
+    encoded = urlencode(auth_params)
+
+    return Redirect(
+        path=f"https://steamcommunity.com/openid/login?{encoded}",
+        status_code=303,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+
+
+@get("/provision_handler", media_type=MediaType.HTML)
+def provision_handler(request: Request) -> str:
+    """Handle a response from Steam.
+
+    Mostly stolen from https://github.com/TeddiO/pySteamSignIn/blob/master/pysteamsignin/steamsignin.py
+
+    Args:
+        request: key value request params from the steam sign in to check against.
+
+    Returns:
+        Page of HTML for user.
+    """
+    data = request.query_params
+    validation_args = {
+        "openid.assoc_handle": data["openid.assoc_handle"],
+        "openid.signed": data["openid.signed"],
+        "openid.sig": data["openid.sig"],
+        "openid.ns": data["openid.ns"],
+    }
+
+    signed_args = data["openid.signed"].split(",")
+    for item in signed_args:
+        arg = f"openid.{item}"
+        if data[arg] not in validation_args:
+            validation_args[arg] = data[arg]
+
+    validation_args["openid.mode"] = "check_authentication"
+    parsed_args = urlencode(validation_args).encode()
+
+    response = requests.get("https://steamcommunity.com/openid/login", params=parsed_args)
+    decoded = response.content.decode()
+    _, valid_str, _ = decoded.split("\n")
+    # valid_str looks like `is_valid:true`
+    valid = bool(valid_str.split(":"))
+
+    if not valid:
+        text = "Could not log you in!"
+
+    else:
+        # great we have the steam id, now lets either provision a new key and display it to the user
+        # if it is not in the DB or say that it already exists, and if they forgot it to let an admin know...
+        # admin will then delete the steam ID of the user in the DB and a new sign in will work.
+        steam_id = os.path.split(data["openid.claimed_id"])[-1]
+
+        print(request.app.state.engine)
+        with request.app.state.engine.connect() as conn:
+            result = conn.execute(
+                sa.text("SELECT * FROM api_keys WHERE steam_id = :steam_id"), {"steam_id": steam_id}
+            ).one_or_none()
+
+            if result is None:
+                api_key = uuid4().int
+                created_at = datetime.now().astimezone(timezone.utc).isoformat()
+                updated_at = created_at
+                conn.execute(
+                    sa.text(
+                        "INSERT INTO api_keys (steam_id, api_key, created_at, updated_at) VALUES (:steam_id, :api_key, :created_at, :updated_at);"  # noqa
+                    ),
+                    {"steam_id": steam_id, "api_key": api_key, "created_at": created_at, "updated_at": updated_at},
+                )
+                conn.commit()  # commit changes...
+                text = f"You have successfully been authenticated! Your API key is {api_key}! Do not lose this as the client needs it!"  # noqa
+
+            else:
+                text = f"Your steam id of {steam_id} already exists in our DB! If you forgot your API key, please let an admin know."  # noqa
+
+    return f"""
+        <html>
+            <body>
+                <div>
+                    <span>{text}</span>
+                </div>
+            </body>
+        </html>
+        """
+
+
 app = Litestar(
     on_startup=[get_db_connection],
-    route_handlers=[session_id, session_id_active, close_session, demo_session],
+    route_handlers=[session_id, session_id_active, close_session, demo_session, provision, provision_handler],
     on_shutdown=[close_db_connection],
 )
