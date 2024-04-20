57f4c1405b73977d6535cbe925863474dc825f9e
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-04-20 17:04:09 +1000

    create bat updates

--- create.bat
+++ create.bat
@@ -7,6 +7,7 @@ set "POSTGRES_PASSWORD=admin"
 set "POSTGRES_HOST=172.20.1.10"
 set "POSTGRES_PORT=5432"
 set "API_HOST=172.20.1.20"
+set "SUBNET=172.20.0.0/16"
 
 :: Check for "--replace" argument
 if "%~1"=="--replace" (
@@ -27,10 +28,10 @@ if "%~1"=="--development" (
 )
 
 pdm sync -G:all
-docker network create --driver bridge masterbase-network-dev --subnet=%API_HOST%/16
+docker network create --driver bridge masterbase-network-dev --subnet=%SUBNET%
 docker build -f Dockerfile.db . -t db-dev
 docker create --name masterbase-db-dev --network masterbase-network-dev --ip %POSTGRES_HOST% -p 8050:%POSTGRES_PORT% -e POSTGRES_USER=%POSTGRES_USER% -e POSTGRES_PASSWORD=%POSTGRES_PASSWORD% -e POSTGRES_DB=demos -t db-dev
 docker start masterbase-db-dev
-docker build -f Dockerfile.api . --build-arg DEVELOPMENT=true -t api-dev
+docker build -f Dockerfile.api . --build-arg DEVELOPMENT=%DEVELOPMENT% -t api-dev
 docker create --name masterbase-api-dev --network masterbase-network-dev --ip %API_HOST% -p 8000:8000 -e POSTGRES_USER=%POSTGRES_USER% -e POSTGRES_PASSWORD=%POSTGRES_PASSWORD% -e POSTGRES_HOST=%POSTGRES_HOST% -e POSTGRES_PORT=%POSTGRES_PORT% -t api-dev
 docker start masterbase-api-dev
