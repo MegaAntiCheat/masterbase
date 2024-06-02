7371208020f319024033bdbc4a91a8fa618a0426
jayceslesar <jaycesles@gmail.com>
2024-06-02 18:14:10 -0400

    fix(fake_ip): unquote

--- masterbase/guards.py
+++ masterbase/guards.py
@@ -1,5 +1,7 @@
 """Guards for the application."""
 
+from urllib.parse import unquote
+
 from litestar.connection import ASGIConnection
 from litestar.exceptions import NotAuthorizedException, PermissionDeniedException
 from litestar.handlers.base import BaseRouteHandler
@@ -85,7 +87,7 @@ async def valid_session_guard(connection: ASGIConnection, _: BaseRouteHandler) -
         return
 
     api_key = get_steam_api_key()
-    fake_ip = connection.query_params["fake_ip"]
+    fake_ip = unquote(connection.query_params["fake_ip"])
 
     to_resolve, port = fake_ip.split(":")
     fake_ip = f"{resolve_hostname(fake_ip)}:{port}"
