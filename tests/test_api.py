9eab983442f8a38f1fd6c590d185b98dc9d5e376
jayceslesar <jaycesles@gmail.com>
2024-06-04 08:11:48 -0400

    full end to end test

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,11 +6,11 @@ from typing import Iterator
 import pytest
 import sqlalchemy as sa
 from litestar import Litestar
-from litestar.response import Stream
-from litestar.status_codes import HTTP_200_OK, HTTP_403_FORBIDDEN
+from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_403_FORBIDDEN
 from litestar.testing import TestClient
 
 from masterbase.app import app
+from masterbase.lib import LATE_BYTES_END, LATE_BYTES_START
 
 pytestmark = pytest.mark.integration
 
@@ -60,10 +60,11 @@ def _send_demo_file(test_client: TestClient[Litestar], api_key: str, session_id:
         time.sleep(5)
         with open("tests/data/test_demo.dem", "rb") as f:
             while True:
-                chunk = f.read(2048)
+                chunk = f.read(720896)
                 if not chunk:
                     break
                 socket.send_bytes(chunk)
+                time.sleep(1)
             socket.close()
 
 
@@ -76,8 +77,14 @@ def test_demo_streaming(test_client: TestClient[Litestar], api_key: str) -> None
     session_id = session_id_response.json()["session_id"]
     _send_demo_file(test_client, api_key, session_id)
 
-    response = test_client.get("/close_session", params={"api_key": api_key})
-    assert response.status_code == HTTP_200_OK
+    late_bytes_hex = "7031cf44a7af0100cea70100f5e00400"
+    late_bytes_response = test_client.post(
+        "/late_bytes", params={"api_key": api_key}, json={"late_bytes": late_bytes_hex}
+    )  # noqa
+    assert late_bytes_response.status_code == HTTP_201_CREATED
+
+    close_session_response = test_client.get("/close_session", params={"api_key": api_key})
+    assert close_session_response.status_code == HTTP_200_OK
 
     # this fails :(
     with test_client.stream("GET", "/demodata", params={"api_key": api_key, "session_id": session_id}) as demo_stream:
@@ -85,4 +92,5 @@ def test_demo_streaming(test_client: TestClient[Litestar], api_key: str) -> None
 
     with open("tests/data/test_demo.dem", "rb") as f:
         demo_in = f.read()
-        assert len(demo_in) == len(demo_out)
+        demo_in = demo_in[:LATE_BYTES_START] + bytes.fromhex(late_bytes_hex) + demo_in[LATE_BYTES_END:]
+        assert demo_in == demo_out
