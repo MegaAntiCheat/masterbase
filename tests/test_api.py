5b35a29e3e41ad0c2121c756e7d9a5647d5ede4f
jayceslesar <jaycesles@gmail.com>
2024-06-03 21:34:20 -0400

    do we got an engine?

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,13 @@
 """Integration tests."""
 
 import pytest
+from masterbase.lib import make_db_uri
+from litestar.testing import TestClient
+from masterbase.app import app
 
 pytestmark = pytest.mark.integration
 
 
-def test_foo():
-    assert "foo" == "bar"
+def test_health_check():
+    with TestClient(app=app) as client:
+        print(app.state.engine)
