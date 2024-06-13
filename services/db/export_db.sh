8f2c92acac59a5332e65583ff0a80c77663172db
kavorite <kavorite@gmail.com>
2024-06-12 23:47:30 -0400

    tidy up a bit

--- services/db/export_db.sh
+++ services/db/export_db.sh
@@ -4,14 +4,15 @@ function execute () {
     psql -U $POSTGRES_USER -d $POSTGRES_NAME -h $POSTGRES_HOST -Atc "$1"
 }
 
+TABLE='demo_sessions'
 COLUMNS=$(execute <<-SQL
     SELECT column_name
     FROM information_schema.columns
-    WHERE table_name = 'demo_sessions'
+    WHERE table_name = '$TABLE'
     AND data_type != 'bytea';
 SQL
 )
-execute <<-SQL | gzip | mc pipe blobs/db_exports/demo_sessions.csv.gz
+execute <<-SQL | gzip | mc pipe blobs/db_exports/$TABLE.csv.gz
     \COPY $TABLE TO STDOUT WITH CSV HEADER
 SQL
 done
