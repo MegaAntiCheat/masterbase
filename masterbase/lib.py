b3cc9a213f8641b5127c13dde9162740fb97cebb
jayceslesar <jaycesles@gmail.com>
2024-07-21 08:24:20 -0400

    feat(core): remove beta tester requirement

--- masterbase/lib.py
+++ masterbase/lib.py
@@ -646,16 +646,6 @@ def update_api_key(engine: Engine, steam_id: str, new_api_key: str) -> None:
         conn.commit()
 
 
-def check_steam_id_is_beta_tester(engine: Engine, steam_id: str) -> bool:
-    """Check that a given steam id has an API key or not."""
-    with engine.connect() as conn:
-        result = conn.execute(
-            sa.text("SELECT * FROM beta_tester_steam_ids WHERE steam_id = :steam_id"), {"steam_id": steam_id}
-        ).one_or_none()
-
-        return bool(result)
-
-
 def provision_api_key(engine: Engine, steam_id: str, api_key: str) -> None:
     """Provision an API key."""
     with engine.connect() as conn:
